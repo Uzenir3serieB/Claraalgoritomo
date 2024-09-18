0c25973fc27b2b80c1eb38aa858269eac3154431
Uzenir3serieB <uzenir3serieB@gmail.com>
2024-09-18 10:26:52 -0300

    aula 4

---
+++ script.js
@@ -0,0 +1,9 @@
+const avanca = document.querySelectorA11(".btn-proximo");
+
+avanca.ForEach (Button => 
+    Button.addEventListener ("click",function) ()
+    const atual = document."passo-" + this.getAttribute("data-proximo");
+
+
+    atual.classList.remove("ativo");
+    document.getElementById
